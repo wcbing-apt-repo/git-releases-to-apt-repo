c3e0b635c4a99e18282610e20b408c3ce46149ba
wcbing <cbing2002@outlook.com>
2025-11-06 17:19:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+# Git Release to APT repo
+
+这是 [wcbing APT Repo](https://github.com/wcbing/wcbing-apt-repo) 的一部分。
+
+### Github
+
+| 软件名 | 包名 | amd64 | arm64 |
+| ----- | ---- | ----- | ----- |
+| [Clash Verge Rev](https://github.com/clash-verge-rev/clash-verge-rev) | clash-verge | ✅ | ✅ |
+| [FlClash](https://github.com/chen08209/FlClash) | flclash | ✅ | ✅ |
+| [mihomo](https://github.com/MetaCubeX/mihomo) | mihomo | ✅ | ✅ |
+| [hugo](https://github.com/gohugoio/hugo) | hugo | ✅ | ✅ |
+| [RustDesk](https://github.com/rustdesk/rustdesk) | rustdesk | ✅ | ✅ |
+| [Obsidian](https://github.com/obsidianmd/obsidian-releases) | obsidian | ✅ | |
+| [Foliate](https://github.com/johnfactotum/foliate) | foliate |  ✅ | ✅ |
+| [Tabby](https://github.com/Eugeny/tabby) | tabby-terminal | ✅ | ✅ |
+| [Pandoc](https://github.com/jgm/pandoc) | pandoc | ✅ | ✅ |
+| [LocalSend](https://github.com/localsend/localsend) | localsend | ✅ | ✅ |
+| [Motrix](https://github.com/agalwood/Motrix) | motrix | ✅ | ✅ |
+| [PeaZip](https://github.com/peazip/PeaZip) | peazip | ✅ | |
+| [Neovim/Nvim](https://github.com/neovim/neovim-releases) | neovim | ✅ | |
+| [Hiddify](https://github.com/hiddify/hiddify-app) | hiddify | ✅ | |
+| [Cloudflare Tunnel](https://github.com/cloudflare/cloudflared) | cloudflared | ✅ | ✅ |
+| [Caddy](https://github.com/caddyserver/caddy) | caddy | ✅ | ✅ |
+| [code-server](https://github.com/coder/code-server) | code-server | ✅ | ✅ |
+| [DBeaver](https://github.com/dbeaver/dbeaver) | dbeaver-ce | ✅ | |
+| [Joplin](https://github.com/laurent22/joplin) | joplin | ✅ | |
+| [SPlayer](https://github.com/imsyy/SPlayer) | splayer | ✅ | |
+| [Tiny RDM](https://github.com/tiny-craft/tiny-rdm) | tinyrdm | ✅ | |
+| [MQTTX](https://github.com/emqx/MQTTX) | mqttx | ✅ | ✅ |
+| [WinBoat](https://github.com/TibixDev/winboat) | winboat | ✅ | |
+| [fooyin](https://github.com/fooyin/fooyin) | fooyin | ✅ | |
+| [Simplenote](https://github.com/Automattic/simplenote-electron) | simplenote | ✅ | ✅ |
+| [YesPlayMusic](https://github.com/qier222/YesPlayMusic) | yesplaymusic | ✅ | ✅ |
+| [Sparkle](https://github.com/xishang0128/sparkle) | sparkle | ✅ | ✅ |
+
+#### Gitee
+
+| 软件名 | 包名 | amd64 | arm64 |
+| ----- | --- | ----- | ----- |
+| [星火应用商店](https://gitee.com/spark-store-project/spark-store) | spark-store | ✅ | ✅ |
+
---
+++ get-git-releases.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+import json
+import logging
+import os
+import re
+import requests
+import subprocess
+import threading
+from concurrent.futures import ThreadPoolExecutor, wait
+
+
+# 全局配置变量
+CONFIG = {
+    "data_dir": "data",
+    "deb_dir": "deb",
+    "packages_dir": "packages",
+    "thread": 5,
+    "dry_run": False,
+}
+version_lock = threading.Lock()
+# 日志等级，若需要展示每次请求结果请使用 INFO 等级
+logging.basicConfig(level=logging.INFO)
+
+
+# 读取 JSON 文件
+def read_json(filename: str) -> dict:
+    try:
+        with open(os.path.join(CONFIG["data_dir"], filename), "r") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        logging.error(e)
+        return {}
+
+
+# 获取最新标签
+def latest_releases_tag(repo_url: str) -> str:
+    url = f"{repo_url}/releases/latest"
+    try:
+        location = requests.head(url).headers.get("Location", "")
+        match = re.search(r".*releases/tag/([^/]+)", location)
+        return match.group(1) if match else ""
+    except requests.RequestException as e:
+        logging.error(e)
+        return ""
+
+
+# 下载文件
+def download(url: str, file_path: str) -> bool:
+    # 检查是否为 dry-run 模式
+    method = requests.head if CONFIG["dry_run"] else requests.get
+    response = method(url, stream=True, allow_redirects=True)
+    if response.status_code != 200:
+        logging.error(f"Can't download {url} because received {response.status_code}")
+        return False
+
+    if CONFIG["dry_run"]:
+        logging.info(f"Dry-run download: {url}")
+    else:
+        logging.info(f"Downloading: {url}")
+        with open(file_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return True
+
+
+def scan(name, arch, url, file_path) -> bool:
+    scan_process = subprocess.run(
+        ["apt-ftparchive", "packages", file_path], capture_output=True
+    )
+    package = scan_process.stdout.decode()
+    package = re.sub(
+        r"^(Filename: ).*", f"\\1{url}", package, flags=re.MULTILINE
+    )  # 替换 Filename 开头的行
+
+    package_file_path = os.path.join(CONFIG["packages_dir"], arch, f"{name}.package")
+
+    try:
+        with open(package_file_path, "w") as f:
+            f.write(package)
+            return True
+    except IOError as e:
+        logging.error(f"Failed to write package file for {name}: {e}")
+        return False
+
+
+# 检查版本并下载新版本文件
+def check(name: str, repo: dict, version_list: dict) -> None:
+    if "site" in repo:
+        repo_url = os.path.join(repo["site"], repo["repo"])
+    else:
+        # 默认认为是 GitHub 仓库地址
+        repo_url = os.path.join("https://github.com", repo["repo"])
+    releases_tag = latest_releases_tag(repo_url)
+    if not releases_tag:
+        logging.error(f"Can't get latest releases tag of {name}")
+        return
+    logging.info(f"{name} = {releases_tag}")
+
+    # https://www.debian.org/doc/manuals/debmake-doc/ch06.zh-cn.html#name-version
+    # 参考文档中 Upstream version 正则表达式获取版本号，即从第一个数字开始。
+    vpattern = "[0-9][-+.:~a-z0-9A-Z]*"
+    version = match.group() if (match := re.search(vpattern, releases_tag)) else ""
+
+    # 判断是否需要更新
+    local_version = version_list.get(name, "")
+    if not version or local_version == version:
+        return
+
+    name = repo["package_name"] if "package_name" in repo else name
+    for arch, file_template in repo["file_list"].items():
+        # 确定本地文件目录并确保目录存在
+        app_dir = os.path.join(CONFIG["deb_dir"], name)
+        os.makedirs(app_dir, exist_ok=True)
+        # 拼接得到 Releases 中的文件名
+        release_filename = file_template.format(
+            releases_tag=releases_tag,  # 若存在则用完整 tag 替换
+            version=version,  # 若存在则用 version 替换
+        )
+        url = f"{repo_url}/releases/download/{releases_tag}/{release_filename}"
+        filename = f"{name}_{version}_{arch}.deb"  # 格式化后的文件名
+        file_path = os.path.join(app_dir, filename)
+
+        # download and scan
+        logging.info(f"Downloading {name}:{arch} ({version})")
+        os.makedirs(os.path.join(CONFIG["deb_dir"], arch), exist_ok=True)
+        if not download(url, file_path):
+            continue
+        logging.info(f"Downloaded {name}:{arch} ({version})")
+        os.makedirs(os.path.join(CONFIG["packages_dir"], arch), exist_ok=True)
+        if not scan(name, arch, url, file_path):
+            continue
+        # 判断是否是新添加应用
+        if local_version == "":
+            print(f"AddNew: {name}:{arch} ({version})")
+        else:
+            print(f"Update: {name}:{arch} ({local_version} -> {version})")
+            # 删除旧版本文件
+            old_file_path = os.path.join(app_dir, f"{name}_{local_version}_{arch}.deb")
+            if os.path.exists(old_file_path):
+                os.remove(old_file_path)
+        # 更新版本号
+        with version_lock:
+            version_list[name] = version
+
+
+if __name__ == "__main__":
+    git_repo_list = read_json("git-repo.json")
+    version_list = read_json("local-version.json")
+    with ThreadPoolExecutor(max_workers=CONFIG["thread"]) as executor:
+        tasks = [
+            executor.submit(check, name, repo, version_list)
+            for name, repo in git_repo_list.items()
+        ]
+        wait(tasks)
+    # 保存到 local-version.json
+    with open(os.path.join(CONFIG["data_dir"], "local-version.json"), "w") as f:
+        json.dump(version_list, f, indent=4)
